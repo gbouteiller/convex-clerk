d7d360d05d393caa46da3de4327f088f9d8a9fcf
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2025-08-29 16:57:21 +0400

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,11 +2,14 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ComponentProps } from "react";
+import { Providers as Pv } from "./(auth)/providers";
 
 export function Providers({ children, ...props }: ComponentProps<typeof NextThemesProvider>) {
 	return (
-		<NextThemesProvider {...props} attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-			{children}
-		</NextThemesProvider>
+		<Pv>
+			<NextThemesProvider {...props} attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+				{children}
+			</NextThemesProvider>
+		</Pv>
 	);
 }
